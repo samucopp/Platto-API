f836d230bb85f5c06dafc748521e5418c34c43a7
samucop95 <samucop@icloud.com>
2024-11-26 16:23:50 +0100

    archivo de errores creado

---
+++ src/helpers/errors.js
@@ -0,0 +1,43 @@
+class USER_NOT_FOUND extends Error {
+    constructor() {
+        super("Usuario no encontrado");
+        this.status = 404;
+    }
+}
+class PASSWORD_NOT_MATCH extends Error {
+    constructor() {
+        super("Contraseñas no coinciden");
+        this.status = 400;
+    }
+}
+
+class USER_ALREADY_EXISTS extends Error {
+    constructor() {
+        super("El usuario ya existe");
+        this.status = 409;
+    }
+}
+
+class USERNAME_ALREADY_EXISTS extends Error {
+    constructor() {
+        super("El nombre de usuario ya está en uso");
+        this.status = 409;
+    }
+}
+
+class INVALID_CREDENTIALS extends Error {
+    constructor() {
+        super("Credenciales inválidas");
+        this.status = 401;
+    }
+}
+
+export const errors = {
+    USER_NOT_FOUND,
+    PASSWORD_NOT_MATCH,
+    USER_ALREADY_EXISTS,
+    USERNAME_ALREADY_EXISTS,
+    INVALID_CREDENTIALS,
+};
+
+export default errors;
