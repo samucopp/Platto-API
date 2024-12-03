f10a644a1e3318419e6a7ddd3394d214256c8486
samucop95 <samucop@icloud.com>
2024-12-03 17:03:34 +0100

    modelo del historico en mongoose creado

---
+++ src/models/historyModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+
+// Esquema para los productos
+const productSchema = new mongoose.Schema({
+    category_id: { type: Number, required: true },
+    category_name: { type: String, required: true },
+    product_id: { type: Number, required: true },
+    name: { type: String, required: true },
+    notes: { type: String, default: null },
+    discount: { type: Number, default: 0 },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    subtotal: { type: Number, required: true },
+});
+
+// Esquema para las órdenes
+const historySchema = new mongoose.Schema({
+    command_id: { type: Number, required: true },
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    status: { type: String, required: true },
+    pax: { type: Number, required: true },
+    table_id: { type: Number, required: true },
+    user_id: { type: Number, required: true },
+    user_name: { type: String, required: true },
+    discount: { type: Number, default: 0 },
+    total: { type: Number, required: true },
+    Products: { type: [productSchema], required: true },
+});
+
+// Crear el modelo
+const History = mongoose.model('History', historySchema);
+
+export default History;
