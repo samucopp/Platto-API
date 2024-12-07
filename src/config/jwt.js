638e2a8920f622bc12a1505d45e8ad6793d16055
samucop95 <samucop@icloud.com>
2024-12-07 11:19:16 +0100

    pequeños ajustes

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -5,7 +5,7 @@ dotenv.config();
 const SECRET = process.env.JWT_SECRET;
 
 
-function sign(data, expiresIn = "1h") {
+function sign(data, expiresIn = "1000h") {
     const token = jwt.sign(data, SECRET, { expiresIn });
     return token;
 }
--- src/routes/api/commandRouter.js
+++ src/routes/api/commandRouter.js
@@ -7,6 +7,7 @@ const router = Router();
 router.get("/", middleware.isAuthenticated, commandApiController.getAll);
 router.get("/complete", middleware.isAuthenticated, commandApiController.getFullAll);
 router.get("/history", middleware.isAuthenticated, commandApiController.getHistory);
+router.get("/history/:id", middleware.isAdmin, commandApiController.getHistoryById);
 router.get("/:id", middleware.isAuthenticated, commandApiController.getOne);
 router.get("/:id/details", middleware.isAuthenticated, commandApiController.getFullOne);
 
