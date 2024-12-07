1cf8aa8452467000f0d24991ea5eb871b0473239
samucop95 <samucop@icloud.com>
2024-12-07 11:15:33 +0100

    cors y swagger instalados

--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
 import express from 'express'; // framework para crear el servidor
 import dotenv from 'dotenv'; // cargar variables de entorno
-import session from 'express-session';// para crear sesiones
 import router from './routes/router.js';// importar rutas
 import db from './config/mongoose.js';
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json" assert { type: 'json' };
+import cors from 'cors';
 
 dotenv.config();// cargar variables de entorno
 
 const app = express();// crear servidor 
 
-app.set('view engine', 'pug');// configurar motor de plantillas
-app.set('src/views',  'views');// configurar directorio de plantillas
-
 app.use(express.static('src/public')); // configurar directorio de archivos estáticos
-app.use(express.urlencoded({ extended: true }));// configurar body parser para recibir datos de formularios
-app.use(express.json());// configurar body parser para recibir datos en formato json
 
-/* app.use(session({
-    secret: process.env.SECRET,// clave para cifrar la cookie
-    resave: false,// no guardar la cookie en cada petición
-    saveUninitialized: false,// no guardar la cookie si no se inicializa
-    cookie: { 
-        secure: false,// true para https
-        maxAge: 1000 * 60 * 60 * 24 * 7// tiempo de vida de la cookie en milisegundos
-        }
-}));// configurar sesión  */
+app.use(express.urlencoded({ extended: true }));// configurar body parser para recibir datos de formularios
+
+app.use(express.json());// configurar body parser para recibir datos en formato json
 
 app.use('/', router);// configurar rutas
 
-app.listen(3000, () => {
-    console.log(`Servidor escuchando en http://localhost:3000`);
-});// iniciar servidor en el puerto indicado en las variables de entorno
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(cors());
+
+app.listen(3000, () => {console.log(`Servidor escuchando en http://localhost:3000`);});
+// iniciar servidor en el puerto indicado en las variables de entorno
